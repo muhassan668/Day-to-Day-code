a965bb5099e05d02520e0a84cfa1f11289942d51
Muhammad Hassan <muhassan668@gmail.com>
2026-02-11 19:54:56 +0500

    Rename files from lab1 to lab2 and lab3

--- CS112/lab1/Lab1_Task1.cpp
+++ CS112/lab1/Lab1_Task1.cpp
@@ -30,11 +30,11 @@ int main(){
 	cout<<"Computer 1: ";
 	cout<<"Brand Name: "<<c1.brand_name<<endl;
 	cout<<"Model Year: "<<c1.model_year<<endl;
-	cout<<"Price: "<<c1.Price<<endl;
+	cout<<"Price     : "<<c1.Price<<endl;
 	cout<<"Computer 2: ";
 	cout<<"Brand Name: "<<c2.brand_name<<endl;
 	cout<<"Model Year: "<<c2.model_year<<endl;
-	cout<<"Price: "<<c2.Price<<endl;
+	cout<<"Price   : "<<c2.Price<<endl;
 	
 	return 0;
 }
